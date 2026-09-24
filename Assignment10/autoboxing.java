6ec0a9f65605f27810ec7792dc22a43633ff65c5
Divyanshu Rawat <270849772+divyanshuiilm@users.noreply.github.com>
2026-09-24 19:10:14 +0530

    Add autoboxing example in autoboxing.java

---
+++ Assignment10/autoboxing.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int no = 100;
+        double p =54.5;
+        char c = 'A';
+        boolean bool = true;
+
+        Integer number = no;
+        Double price = p;
+        Character characte = c;
+        Boolean status = bool;
+
+        System.out.println("Integer = " + number);
+        System.out.println("Double = " + price);
+        System.out.println("Character = " + characte);
+        System.out.println("Boolean = " + status);
+    }
+}
