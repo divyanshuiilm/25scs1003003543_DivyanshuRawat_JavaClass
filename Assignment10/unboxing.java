3b08e4ff0b535d9287b8fe5b40b70e7767c9e44c
Divyanshu Rawat <270849772+divyanshuiilm@users.noreply.github.com>
2026-09-24 19:09:35 +0530

    Create unboxing.java

---
+++ Assignment10/unboxing.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Integer number = 100;
+        Double price = 54.5;
+        Character characte = 'A';
+        Boolean status = true;
+
+        int no = number;
+        double p = price;
+        char c = characte;
+        boolean bool = status;
+
+        System.out.println("int = " + no);
+        System.out.println("double = " + p);
+        System.out.println("char = " + c);
+        System.out.println("boolean = " + bool);
+    }
+}
