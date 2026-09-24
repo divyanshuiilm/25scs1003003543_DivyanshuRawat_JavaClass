e3c53f1b52385c86a26482e4158c5024da381c99
Divyanshu Rawat <270849772+divyanshuiilm@users.noreply.github.com>
2026-09-24 19:11:43 +0530

    Add wrapperclass.java to demonstrate primitives and wrappers
    Demonstrates usage of primitive and wrapper classes in Java.

---
+++ Assignment10/wrapperclass.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Primitive variables
+        byte b = 10;
+        short s = 20;
+        int i = 30;
+        long l = 40L;
+        float f = 50.5f;
+        double d = 60.5;
+        char c = 'A';
+        boolean e = true;
+
+        // Wrapper variables
+        Byte bb = 10;
+        Short ss = 20;
+        Integer ii = 30;
+        Long ll = 40L;
+        Float ff = 50.5f;
+        Double dd = 60.5;
+        Character cc = 'A';
+        Boolean ee = true;
+
+        System.out.println("Primitive Values:");
+        System.out.println("byte = " + b);
+        System.out.println("short = " + s);
+        System.out.println("int = " + i);
+        System.out.println("long = " + l);
+        System.out.println("float = " + f);
+        System.out.println("double = " + d);
+        System.out.println("char = " + c);
+        System.out.println("boolean = " + e);
+
+        System.out.println("\nWrapper Values:");
+        System.out.println("Byte = " + bb);
+        System.out.println("Short = " + ss);
+        System.out.println("Integer = " + ii);
+        System.out.println("Long = " + ll);
+        System.out.println("Float = " + ff);
+        System.out.println("Double = " + dd);
+        System.out.println("Character = " + cc);
+        System.out.println("Boolean = " + e);
+    }
+}
